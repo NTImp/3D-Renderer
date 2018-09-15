ebfee92665e1b2dcf45b98662051aca01c5a72cc
Jose Meroño <josesmerono@gmail.com>
2018-09-15 12:43:31 +0200

    Optimized Rasterized and deleted unused files

--- src/Render3D/Renderer.cpp
+++ src/Render3D/Renderer.cpp
@@ -66,6 +66,10 @@ namespace Render3D {
 			ProjOutTriangle& ot = clipOut[clipCount];
 			ot = it;
 
+			if (	ot.t[0].position.w < 0.1 ||
+				ot.t[1].position.w < 0.1 ||
+				ot.t[2].position.w < 0.1) continue;
+
 			ot.t[0].position.x /= ot.t[0].position.w;
 			ot.t[0].position.y /= ot.t[0].position.w;
 			
@@ -75,17 +79,35 @@ namespace Render3D {
 			ot.t[2].position.x /= ot.t[2].position.w;
 			ot.t[2].position.y /= ot.t[2].position.w;
 
+			/*
 			bool visible = true;
 			for (int e = 0; e < 3 && visible; e++)
 			{
 				if (	ot.t[e].position.z < 0.1 ||
-					ot.t[e].position.x < -1   ||
+					ot.t[e].position.x < -1  ||
 					ot.t[e].position.x > 1   ||
-					ot.t[e].position.y < -1   ||
+					ot.t[e].position.y < -1  ||
 					ot.t[e].position.y > 1) visible = false;
 			}
 
 			if (!visible) continue;
+			*/
+
+			if (	ot.t[0].position.x < -1 &&
+				ot.t[1].position.x < -1 &&
+				ot.t[1].position.x < -1) continue;
+
+			if (	ot.t[0].position.x > 1 &&
+				ot.t[1].position.x > 1 &&
+				ot.t[1].position.x > 1) continue;
+
+			if (	ot.t[0].position.y < -1 &&
+				ot.t[1].position.y < -1 &&
+				ot.t[1].position.y < -1) continue;
+
+			if (	ot.t[0].position.y > 1 &&
+				ot.t[1].position.y > 1 &&
+				ot.t[1].position.y > 1) continue;
 
 			//Face culling
 			Math::Vector2 v1;
@@ -109,6 +131,11 @@ namespace Render3D {
 				} else continue;
 			} else if (fcull == CullType::back && back) continue;
 
+			/*
+			ot.t[0].position.y *= -1;
+			ot.t[0].position.y *= -1;
+			ot.t[0].position.y *= -1;
+			*/
 			ot.t[0].position.x += 1;
 			ot.t[0].position.y += 1;
 
@@ -131,14 +158,28 @@ namespace Render3D {
 			clipCount++;
 		}
 
-#define Edge(px, py, a, b) (((float)px - a.x) * (b.y - a.y) - ((float)py - a.y) * (b.x - a.x))
+#define Edge(px, py, a, b) ((px - a.x) * (b.y - a.y) - (py - a.y) * (b.x - a.x))
 		for (int i = 0; i < clipCount; i++) {
-			ProjOutTriangle& t = clipOut[i];
+			ProjOutTriangle t = clipOut[i];
 			int minx = w;
 			int miny = h;
 			int maxx = 0;
 			int maxy = 0;
-	
+
+			/*
+			t.t[2].position.x = 100;
+			t.t[2].position.y = 10;
+
+			t.t[1].position.x = w - 1;
+			t.t[1].position.y = 0;
+
+			t.t[0].position.x = 0;
+			t.t[0].position.y = h - 1;
+
+			t.t[0].position.z = 1;
+			t.t[1].position.z = 1;
+			t.t[2].position.z = 1;
+			*/
 			//Calculate the minimum rectangle (in clip coordinates) thar contains the triangle
 			for(int i = 0; i < 3; i ++) {
 				minx = std::min((int) t.t[i].position.x, minx);
@@ -154,30 +195,32 @@ namespace Render3D {
 			int sminy = std::max(0, miny);
 			int smaxy = std::min(h, maxy + 1);
 
-			float y = sminy;
+			int a01 = t.t[0].position.y - t.t[1].position.y;
+			int a12 = t.t[1].position.y - t.t[2].position.y;
+			int a20 = t.t[2].position.y - t.t[0].position.y;
+
+			int b01 = t.t[1].position.x - t.t[0].position.x;
+			int b12 = t.t[2].position.x - t.t[1].position.x;
+			int b20 = t.t[0].position.x - t.t[2].position.x;
+
+			int e01_r = Edge(sminx, sminy, t.t[0].position, t.t[1].position);
+			int e12_r = Edge(sminx, sminy, t.t[1].position, t.t[2].position);
+			int e20_r = Edge(sminx, sminy, t.t[2].position, t.t[0].position);
+
 			for (int i = sminy; i < smaxy; i++) {
-				float x = sminx;
+				int e01 = e01_r;
+				int e12 = e12_r;
+				int e20 = e20_r;
+
 				for(int e = sminx; e < smaxx; e++) {
-					float e1 = Edge(x, y, t.t[1].position, t.t[2].position) / 2;
-					float e2 = Edge(x, y, t.t[2].position, t.t[0].position) / 2;
-					float e3 = Edge(x, y, t.t[0].position, t.t[1].position) / 2;
+					if(e01 >= 0 && e20 >= 0 && e12 >= 0) {
+						float area = (float) (e01 + e20 + e12) / 2;
 
+						float depth = 	((float) e12 / area) * t.t[0].position.w +
+								((float) e20 / area) * t.t[1].position.w +
+								((float) e01 / area) * t.t[2].position.w;
 
-					if(e1 > 0 && e2 > 0 && e3 > 0)
-					{
-						float area = e1 + e2 + e3;
-						e1 /= area;
-						e2 /= area;
-						e3 /= area;
-
-						float depth = 	e1 * t.t[0].position.z +
-								e2 * t.t[1].position.z +
-								e3 * t.t[2].position.z;
-
-						float light = 	e1 * t.t[0].light +
-								e2 * t.t[1].light +
-								e3 * t.t[2].light;
-
+						float light = t.t[0].light;
 						if (depth < depthBuffer[i * w + e])
 						{
 							depthBuffer[i * w + e] = depth;
@@ -190,9 +233,13 @@ namespace Render3D {
 							out->data[i * w + e] = pixel;
 						}
 					}
-					x++;
+					e01 -= a01;
+					e20 -= a20;
+					e12 -= a12;
 				}
-				y++;
+				e01_r -= b01;
+				e20_r -= b20;
+				e12_r -= b12;
 			}
 		}
 	}
